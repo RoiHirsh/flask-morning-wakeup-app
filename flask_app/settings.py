8886318e3e75039f11c9fb42ea33ccafe0d1fc58
RoiHirsh <sharpstudio.xyz@gmail.com>
2022-10-06 22:23:53 +0300

    changes

--- flask_app/settings.py
+++ flask_app/settings.py
@@ -1,6 +1,6 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL?sslmode=require').replace('postgres://', 'postgresql://')
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace('postgres://', 'postgresql://')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
