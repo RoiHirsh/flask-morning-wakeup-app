d44365e06dfeee5bc50f1c87ad0fa984f45723cd
RoiHirsh <sharpstudio.xyz@gmail.com>
2022-10-06 22:20:20 +0300

    changes

--- flask_app/settings.py
+++ flask_app/settings.py
@@ -1,8 +1,11 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL?sslmode=require').replace('postgres://', 'postgresql://')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+#save
+#SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 #change to this row below when deloying to production on Heroku:
 #SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL')
