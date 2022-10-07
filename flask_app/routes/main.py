12bd0e52eba16a1f2cc59f944b2412c6a33b917a
RoiHirsh <sharpstudio.xyz@gmail.com>
2022-10-07 10:02:56 +0300

    changes

--- flask_app/routes/main.py
+++ flask_app/routes/main.py
@@ -92,7 +92,7 @@ def dashboard():
                 Schedule.query.filter_by(dt=dt).delete()
             for i in range(len(schedule_hours)):
                 #schedule = Schedule(dt=dt,time=schedule_hours[i],task=schedule_tasks[i])
-                schedule = Schedule(weekday='sunday',time=schedule_hours[i],task=schedule_tasks[i])
+                schedule = Schedule(weekday='monday',time=schedule_hours[i],task=schedule_tasks[i])
                 db.session.add(schedule)
             db.session.commit()
             thanks_schedule = 'מקווים שהנתונים הועלו לדאטהבייס'
